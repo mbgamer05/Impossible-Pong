389b88c0dcbaf4d98f3473eb27556d8eee9776eb
mbgamer05 <31849608+mbgamer05@users.noreply.github.com>
2022-06-08 10:01:26 +1000

    Complete download
    Have fun  guys

---
+++ Ping Pong/Ping Pong/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Ping_Pong
+{
+    partial class frmPong
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPong));
+            this.picComp = new System.Windows.Forms.PictureBox();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.picWall = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picComp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picComp
+            // 
+            this.picComp.BackColor = System.Drawing.Color.Red;
+            this.picComp.Location = new System.Drawing.Point(925, 209);
+            this.picComp.Name = "picComp";
+            this.picComp.Size = new System.Drawing.Size(40, 100);
+            this.picComp.TabIndex = 0;
+            this.picComp.TabStop = false;
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.BackColor = System.Drawing.Color.Lime;
+            this.picPlayer.Location = new System.Drawing.Point(12, 183);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(40, 126);
+            this.picPlayer.TabIndex = 1;
+            this.picPlayer.TabStop = false;
+            this.picPlayer.Click += new System.EventHandler(this.picPlayer_Click);
+            // 
+            // picWall
+            // 
+            this.picWall.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.picWall.Location = new System.Drawing.Point(497, 12);
+            this.picWall.Name = "picWall";
+            this.picWall.Size = new System.Drawing.Size(22, 519);
+            this.picWall.TabIndex = 2;
+            this.picWall.TabStop = false;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.White;
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(351, 194);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(40, 40);
+            this.picBall.TabIndex = 3;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmPong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(977, 543);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.picWall);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.picComp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmPong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmPong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmPong_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picComp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox picComp;
+        private PictureBox picPlayer;
+        private PictureBox picWall;
+        private PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Ping Pong/Ping Pong/Form1.cs
@@ -0,0 +1,148 @@
+namespace Ping_Pong
+{
+    public partial class frmPong : Form
+    {
+        bool goUp;
+        bool goDown;
+        int pPaddleSpeed;
+        int cPaddleSpeed;
+        int pScore;
+        int cScore;
+        int timeytick;
+        Random random;
+        BallXY ballXy;
+        struct BallXY 
+        {
+            public int x;
+            public int y;
+        }
+        public frmPong()
+        {
+            InitializeComponent();
+            cPaddleSpeed = 5;
+            pPaddleSpeed = 5;
+            pScore = 0;
+            cScore = 0;
+            random = new Random();
+            ballXy.x = 5;
+            ballXy.y = 5;
+            timeytick = 1;
+        }
+
+        private void picPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPong_Load(object sender, EventArgs e)
+        {
+            this.Text = "Player Score " + pScore + "        |        Computer Score " + cScore;
+            picPlayer.Left = 0;
+            picPlayer.Top = (ClientSize.Height / 2) - picPlayer.Height / 2;
+            picComp.Left = (ClientSize.Width) - picComp.Width / 2;
+            picComp.Top = (ClientSize.Height / 2) - picComp.Height / 2;
+            picWall.Top = 0;
+            picWall.Left = (ClientSize.Width / 2) - picWall.Width / 2;
+            picWall.Height = ClientSize.Height;
+            picBall.Left = (ClientSize.Width / 2) + 40;
+            picBall.Top = (ClientSize.Height / 2) + 40;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void frmPong_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                picPlayer.BackColor = Color.Yellow;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                picPlayer.BackColor = Color.Blue;
+            }
+        }
+
+        private void frmPong_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+                picPlayer.BackColor = Color.Lime;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+                picPlayer.BackColor = Color.Lime;
+
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Player Score " + pScore + "        |        Computer Score " + cScore;
+            picBall.Left -= ballXy.x;
+            picBall.Top -= ballXy.y;
+            picComp.Top -= cPaddleSpeed;
+            
+            
+            if (picComp.Top < 0 || picComp.Top > ClientSize.Height - picComp.Height)
+            {
+                cPaddleSpeed = -cPaddleSpeed;
+            } 
+            if (picBall.Top < 0 || picBall.Top > ClientSize.Height - picBall.Height)
+            {
+                ballXy.y = -ballXy.y;
+            }
+            if (picBall.Left < 0)
+            {
+                picBall.Left = ClientSize.Width / 2 - 200;
+                picBall.Top = random.Next(20, ClientSize.Height - 20);
+                cScore += 1;
+            }
+            if (picBall.Left > ClientSize.Width - picBall.Width)
+            {
+                picBall.Left = ClientSize.Width / 2 + 200;
+                picBall.Top = random.Next(20, ClientSize.Height - 20);
+                pScore += 1;
+            }
+            if (picBall.Bounds.IntersectsWith(picPlayer.Bounds) || picBall.Bounds.IntersectsWith(picComp.Bounds))
+            {
+                ballXy.x =-ballXy.x;
+                if (picBall.Bounds.IntersectsWith(picPlayer.Bounds))
+                {
+                    picBall.BackColor = Color.LimeGreen;
+                }
+                if (picBall.Bounds.IntersectsWith(picComp.Bounds))
+                {
+                    picBall.BackColor = Color.IndianRed;
+                }
+            }
+            
+            if (picPlayer.Top > 0 && goUp)
+            {
+                picPlayer.Top -= pPaddleSpeed;
+            }
+            if (picPlayer.Top < ClientSize.Height - picPlayer.Height && goDown)
+            {
+                picPlayer.Top += pPaddleSpeed;
+            }
+            if (pScore >= 10)
+            {
+                timer1.Stop();
+                MessageBox.Show("You Win");
+            }
+            if(cScore >= 10)
+            {
+                timer1.Stop();
+                MessageBox.Show("Computer Wins");
+            }
+            timeytick = timeytick + 1 / 2;
+            ballXy.y = ballXy.y + timeytick;
+            ballXy.x = ballXy.x + timeytick;
+        }
+        
+    }
+}
